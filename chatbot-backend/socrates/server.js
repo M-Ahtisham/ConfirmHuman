83d557167c80396ef6581e110f80a08b047b773a
Muhammad Ahtisham Bhatti <muhammad.bhatti2@stud.th-deg.de>
2025-06-19 01:14:42 +0200

    moved files and add socketio

---
+++ chatbot-backend/socrates/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+const intentHandler = require('./intentHandler');
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000', 
+    methods: ['GET', 'POST'],
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('User connected:', socket.id);
+
+  socket.on('user_message', (msg) => {
+    const reply = intentHandler(msg); 
+    socket.emit('bot_message', reply);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
